c93ba58c429af08cbb5cee1df5f0ed185437b795
Suraj Karki <suraj.karki500@protonmail.com>
2025-07-07 00:33:07 +0200

    add a notebook for exploring report

--- setup.sh
+++ setup.sh
@@ -5,11 +5,8 @@ git submodule update --init --recursive
 conda create -n satay python=3.10
 conda activate satay
 cd fpgaconvnet-model
-python -m pip install .
+python3 -m pip install .
 cd ../fpgaconvnet-optimiser
-python -m pip install .
+python3 -m pip install .
 pip install nvidia-pyindex
 pip install onnx-graphsurgeon
-cd ../onnx_models
-./get_yolo_models.sh
-cd ..
